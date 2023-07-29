39f2d03f663ab28e36343acd050e947b9ba0ce7d
Nils Freydank <nils.freydank@posteo.de>
2023-07-29 17:30:39 +0200

    Make version and registry call-able from env for images, too

--- build-image.sh
+++ build-image.sh
@@ -6,8 +6,8 @@ set -uxa
 
 GPG_SIGNING_KEY="${GPG_SIGNING_KEY:-0x0F1DEAB2D36AD112}"
 
-REGISTRY="git.holgersson.xyz/gentoo-related/gentoo-binpkg-builder"
-VERSION="$(date --utc +%Y%m%d_%H%M%S)"
+REGISTRY="${REGISTRY:-git.holgersson.xyz/gentoo-related/gentoo-binpkg-builder}"
+VERSION="${VERSION:-$(date --utc +%Y%m%d_%H%M%S)}"
 IMAGE_TAG="${REGISTRY}:${VERSION}"
 
 REPOS="${REPOS:-/var/db/repos}"
